a75df6d0dd97f86e70890ec1f5b2a20ca55799a8
Shamsulhuda <shamsulhudakhwahanygmail.com>
2026-07-13 00:27:02 +0500

    Variables practice

---
+++ about_me.py
@@ -0,0 +1,7 @@
+name = "Amir"        
+age = 20             
+height = 1.75        
+is_student = True  
+sentences = "MY name is shams "
+print(f"My name is {name}, I am {age}, and I love {sentences} , and my height is {height}.")
+print(age + 10)
